0acb1b6110cdba1ae43cf425ed26d5bda7e0f5f2
gopinath-maharaja <gopinathm@kenlasystems.com>
2021-12-17 11:23:44 +0530

    img upload

--- server.js
+++ server.js
@@ -13,61 +13,64 @@ const PORT = 3000;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Version 1'+process.env.API);
+	res.send('Hello World Version 1' + process.env.API);
 });
 
 app.get('/api', (req, res) => {
-  res.send('Hello World Version 2'+process.env.API);
+	res.send('Hello World Version 2' + process.env.API);
 });
 
 app.get('/api-get', (req, res) => {
-  res.send('Hello Worrrld Version 3 7');
+	res.send('Hello Worrrld Version 3 7');
 });
 
 
 app.get('/api-get-role', (req, res) => {
-  res.send('Hello World Version 3 role get method 7');
+	res.send('Hello World Version 3 role get method 7');
 });
 
 app.get('/api/role', (req, res) => {
-  res.send('role api execute');
+	res.send('role api execute');
 });
 
 app.get('/create', (req, res) => {
-  let path = "./logo.gif"
-  fs.readFile(path, async function (err, data) {
-    let params = { 
-      Bucket: "sample-test-demo-gopi", 
-      Key: Date.now() + '-' + "gopi",
-       Body: data, 
-       serverSideEncryption: 'aws:kms',
-       sseKmsKeyId: "arn:aws:kms:us-west-1:172919200252:key/dffa8ae4-3c53-4619-b5f8-dfe762420ae1" };
-    s3.upload(params, function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });                
-});
-  res.send('success');
+	let path = "./coco.png"
+	fs.readFile(path, async function (err, data) {
+		let params = {
+			Bucket: "sample-test-demo-gopi",
+			Key: Date.now() + '-' + "coco.png",
+			Body: data,
+			serverSideEncryption: 'aws:kms',
+			sseKmsKeyId: "arn:aws:kms:us-west-1:172919200252:key/dffa8ae4-3c53-4619-b5f8-dfe762420ae1"
+		};
+		s3.upload(params, function (err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(data);
+			}
+		});
+	});
+	res.send('success');
 });
 
 
 app.get('/next/:name', (req, res) => {
-  let { name } =req.params;
-var params = {
-  Bucket: "sample-test-demo-gopi", 
-  Key: name
- };
- s3.getObject(params, function(err, data) {
-   if (err){ res.send(err, err.stack);} // an error occurred
-   else { res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-        res.write(data.Body, 'binary');
-        res.end(null, 'binary');
-        data.Body.toString('utf-8');}           // successful response
-  
- });
+	let { name } = req.params;
+	var params = {
+		Bucket: "sample-test-demo-gopi",
+		Key: name
+	};
+	s3.getObject(params, function (err, data) {
+		if (err) { res.send(err, err.stack); } // an error occurred
+		else {
+			res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+			res.write(data.Body, 'binary');
+			res.end(null, 'binary');
+			data.Body.toString('utf-8');
+		}           // successful response
+
+	});
 
 });
 
