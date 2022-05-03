0245690e818ced8a8dc6180aad95dac77a0dd623
gopi-raja-15 <78584322+gopi-raja-15@users.noreply.github.com>
2022-05-03 17:18:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 
 const express = require('express');
 require('dotenv').config();
-const aws = require('aws-sdk');
-const s3 = new aws.S3();
-const fs = require('fs');
 
 // Constants
 const PORT = 3000;
@@ -13,88 +10,34 @@ const PORT = 3000;
 // App
 const app = express();
 app.get('/', (req, res) => {
+	console.log("Hit------------->1")
 	res.send('Hello World Version 1' + process.env.API);
 });
 
 app.get('/api', (req, res) => {
+		console.log("Hit------------->2")
 	res.send('Hello World Version 2' + process.env.API);
 });
 
 app.get('/api-get', (req, res) => {
+		console.log("Hit------------->3")
 	res.send('Hello Worrrld Version 3 7');
 });
 
 
 app.get('/api-get-role', (req, res) => {
+		console.log("Hit------------->4")
 	res.send('Hello World Version 3 role get method 7');
 });
 
 app.get('/api/role', (req, res) => {
-	res.send('role api execute');
+		console.log("Hit------------->5")
+	 res.status(200).json({
+            status: 'success',
+            payload: "ROLE",
+            message: 'ROLE Fetched successfully'
+        });
 });
-
-app.get('/create', (req, res) => {
-	let path = "./coco.png"
-	fs.readFile(path, async function (err, data) {
-		let params = {
-			Bucket: "sample-test-demo-gopi",
-			Key: Date.now() + '-' + "coco.png",
-			Body: data,
-			serverSideEncryption: 'aws:kms',
-			sseKmsKeyId: "arn:aws:kms:us-west-1:172919200252:key/dffa8ae4-3c53-4619-b5f8-dfe762420ae1"
-		};
-		s3.upload(params, function (err, data) {
-			if (err) {
-				console.log(err);
-				res.send(err);
-			} else {
-				console.log(data);
-				res.send(data);
-
-			}
-		});
-	});
-});
-
-app.get('/delete', (req, res) => {
-	var params = {
-		AccountId: '172919200252', /* required */
-		Bucket: 'arn:aws:s3:::sample-test-demo-gopi' /* required */
-	  };
-	  s3.deleteBucketPolicy(params, function(err, data) {
-		if (err) {
-			console.log(err, err.stack); 
-			res.send(err);
-
-		}// an error occurred
-		else     {
-			console.log(data);  
-			res.send(data);
-
-		         }         // successful response
-	  });
-});
-
-
-app.get('/next/:name', (req, res) => {
-	let { name } = req.params;
-	var params = {
-		Bucket: "sample-test-demo-gopi",
-		Key: name
-	};
-	s3.getObject(params, function (err, data) {
-		if (err) { res.send(err, err.stack); } // an error occurred
-		else {
-			res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-			res.write(data.Body, 'binary');
-			res.end(null, 'binary');
-			data.Body.toString('utf-8');
-		}           // successful response
-
-	});
-
-});
-
 
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
